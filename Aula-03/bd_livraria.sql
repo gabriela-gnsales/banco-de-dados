026d9886d66d5e6e9a7ef3e5c94ad9b528d75085
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-04-29 22:41:25 -0300

    Atualização bd_livraria

--- Aula-03/bd_livraria.sql
+++ Aula-03/bd_livraria.sql
@@ -1,58 +1,58 @@
 CREATE TABLE cliente (
-	id_cliente INT PRIMARY KEY,
-	nome_cliente VARCHAR(20) NOT NULL,
-	email_cliente VARCHAR(20) NOT NULL, -- VERIFICAR
-	senha_cliente VARCHAR(8) NOT NULL, 
-	cpf_cliente VARCHAR(11) NOT NULL, 
-	sexo_cliente VARCHAR(20) NOT NULL, -- VERIFICAR    
-	data_nascimento_cliente VARCHAR(30) NOT NULL, -- VERIFICAR
-)
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	email VARCHAR(50) NOT NULL,
+	senha VARCHAR(8) NOT NULL, 
+	cpf CHAR(11) NOT NULL, 
+    data_nascimento DATE NOT NULL,
+	sexo CHAR(1)	 
+);
 
 CREATE TABLE pedido (
-	id_pedido INT PRIMARY KEY,
-	status VARCHAR(10) NOT NULL, -- VERIFICAR
-	data_realizacao ? NOT NULL, -- VERIFICAR
-	prazo_entrega INT NOT NULL, -- VERIFICAR
-)
+	id SERIAL PRIMARY KEY,
+	status VARCHAR(50) NOT NULL,
+	data_realizacao DATE NOT NULL, 
+	prazo_entrega INT NOT NULL
+);
 
 CREATE TABLE livro (
-	id_livro INT PRIMARY KEY,
-	titulo VARCHAR(10) NOT NULL,
-	descricao TEXT NULL, 
-	editora VARCHAR(10) NOT NULL, 
+	id SERIAL PRIMARY KEY,
+	titulo VARCHAR(100) NOT NULL,
 	numero_paginas INT NOT NULL, 
-	preco NUMERIC CHECK(preco > 0) NOT NULL,   
-	formato VARCHAR(10) NOT NULL, -- VERIFICAR
-    quantidade INT NOT NULL, 
-    categoria VARCHAR(15) NOT NULL
-)
+    quantidade INT DEFAULT 0 NOT NULL, 
+	preco NUMERIC(7,2) CHECK(preco > 0) NOT NULL,   
+	formato VARCHAR(50),
+    editora VARCHAR(50), 
+    categoria VARCHAR(50),
+    descricao TEXT
+);
 
 CREATE TABLE autor (
-	id_autor INT PRIMARY KEY,
-	nome_autor VARCHAR(20) NOT NULL,
-	nacionalidade VARCHAR(20) NOT NULL, 
-	data_nascimento_autor VARCHAR(8) NOT NULL, -- VERIFICAR
-	data_falecimento_autor VARCHAR(11) NOT NULL, -- VERIFICAR
-	descricao TEXT NULL
-)
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL,
+	data_nascimento DATE NOT NULL, 
+	data_falecimento DATE, 
+    nacionalidade VARCHAR(50),
+    descricao TEXT
+);
 
 CREATE TABLE endereco (
-	id_endereco INT PRIMARY KEY,
-	logradouro VARCHAR(30) NOT NULL,
-	numero INT NOT NULL, -- VERIFICAR
-	bairro VARCHAR(10) NOT NULL, 
-	cep VARCHAR(11) NOT NULL, -- VERIFICAR
-	complemento VARCHAR(10) NULL,
-	ponto_referencia TEXT NULL
-)
+	id SERIAL PRIMARY KEY,
+	logradouro VARCHAR(100) NOT NULL,
+    bairro VARCHAR(50) NOT NULL, 
+	numero INT NOT NULL, 
+	cep CHAR(8) NOT NULL,
+	complemento VARCHAR(50),
+	ponto_referencia TEXT
+);
 
 CREATE TABLE cidade (
-	id_cidade INT PRIMARY KEY,
-	nome_cidade VARCHAR(15) NOT NULL -- VERIFICAR
-)
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL
+);
 
 CREATE TABLE estado (
-	id_estado INT PRIMARY KEY,
-	nome_estado VARCHAR(10) NOT NULL, -- VERIFICAR
-	sigla VARCHAR(2) NOT NULL
-)
+	id SERIAL PRIMARY KEY,
+	nome VARCHAR(50) NOT NULL, 
+	sigla CHAR(2) NOT NULL
+);
