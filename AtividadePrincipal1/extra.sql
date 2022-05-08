29d18cefa584130bdaa4ec626f83365f83eb41e6
Gabriela Sales <95177458+gabriela-gnsales@users.noreply.github.com>
2022-05-08 20:30:22 -0300

    ok

--- AtividadePrincipal1/extra.sql
+++
@@ -1,5 +0,0 @@
--- 1) Liste o top 3 de funcionários com mais vendas no primeiro trimestre de 1997;
-
--- 2) Conte todos os diferentes fornecedores por país. Agrupe categorizando os países em 'Americas' (se o país for USA, Brazil ou Canada) e 'Outros' senão. Detalhe, estamos buscando aqueles cujo DDD não comece com o dígito 1;
-
--- 3) Tome a primeira letra de cada cidade (ex. "N" para "New Orleans"). Quais são as 5 iniciais de nomes de cidades que têm mais fornecedores associados (em ordem descrescente de fornecedores/cidade)?
